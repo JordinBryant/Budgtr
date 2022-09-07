00e325edfe3af7d419c8360200e4dda02ffa3d79
Jordin Bryant <jordintaylor095@gmail.com>
2022-09-07 16:27:20 -0400

    commit

--- server.js
+++ server.js
@@ -1,36 +1,38 @@
 const express = require("express");
 const app = express();
-const port = 3000;
 const budget = require("./models/budget.js");
 
+app.use(express.urlencoded({extended: false}));
 
-app.use(express.urlencoded({ extended: false }))
 
-app.get("/budgets", (req, res) => {
-    res.render("index.ejs", {
-        allBudgets: budget
+app.get("/", (req, res)=>{
+    res.send("testing");
+});
+
+
+app.get("/budgets", (req, res) =>{
+    res.render("index.ejs",{
+        budget,
     });
 });
 
-app.get("/budgets/new", (req, res) => {
-    res.render("new.ejs")
+
+app.get("/budgets/new", (req, res)=>{
+    res.render("new.ejs");
 });
 
-app.post("/budgets", (req, res) => {
-    budget.push(req.body)
-    res.redirect("/budgets")
+
+app.post("/budgets", (req, res)=>{
+    budget.push(req.body),
+    res.redirect("/budgets");
 });
 
-app.get("/budgets/:index", (req, res) => {
-    res.render("show.ejs", {
-        budgets: budget[req.params.index]
+
+app.get("/budgets/:id", (req, res) =>{
+    res.render("show.ejs" , {
+        budget : budget[req.params.id],
     });
 });
 
-app.get("/", (req, res) => {
-    res.send("Welcome to the Budgtr App!");
-});
 
-app.listen(port, () => {
-    console.log("Express is listening")
-});
+app.listen(3000);
